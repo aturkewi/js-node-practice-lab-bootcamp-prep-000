2a9969e07f8c895185ea010cb07a223b395f3a88
Avidor Turkewitz <avi.turkewitz@flatironschool.com>
2018-07-03 13:07:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,4 @@
 // console.log("Height:")
 // console.log(height)
 // console.log(parseInt(height)+1)
+var height = 84; var Name = "Janet"; var height = "74"; console.log("Name:"); console.log(name); console.log("Height:"); console.log(height); var height = 54; console.log(parseInt(height) + 1);
